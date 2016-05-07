ba964996ee57be4951b2a8c79fef61ec1e35ca15
Mahesh Uligade <maheshuligade@gmail.com>
2016-05-07 13:07:23 +0530

    Added License added.

--- about/index.php
+++ about/index.php
@@ -136,13 +136,67 @@
 			</div>
 
 		</nav>
+<div class = "container" style="padding:100px;text-align:center;	">
+<!--<div class="modal fade" id = "modal_id" role = "dialog">-->
+<!--	<div class = "modal-dialog">-->
+<!--		<div class="modal-content">-->
+			<div class = "model-header" style="text-align:center;">
+				<h1>License</h1>
+			</div>
+			
+<!--		<div class = "modal-body">-->
+<!--			<p>	-->
+				
+				
+<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License</a>.
+<ol style = "text-align:left;">
+<h5 style = "text-align:left;">Authors of this site:</h5> 
+	<li>Mahesh Uligade (maheshuligade@gmail.com)</li>
+</ol>
+	<br/>
+<p>
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met: 
+</p>
+<ol style="text-align:left;">
+<li>Redistributions of source code must retain the above copyright notice, this
+	list of conditions and the following disclaimer. 
+</li>
+<li>Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+	and/or other materials provided with the distribution. 
+</li>
+</ol>
+<p>
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE
+	</p>
+<!--
+<script type = "text/javascript">
+ $('#modal_id').modal('show');				
+</script>
+			</p>
+			</div>		
+		</div>		
+	</div>
+</div>	
+-->
+</div>	
 		<footer class="container-fluid text-center" style="margin-top:30px;padding:0px;margin-bottom:0px;">
 			<p>	
 				<a href="#mainpage" tilte="To Top">
 				<span class="glyphicon glyphicon-chevron-up"></span>
 				</a>
 			</p>
-			
+			<p>
 				<a href="../about"><strong>&copy Robotics Lab</strong></a> 
 			</p>
 		</footer>
